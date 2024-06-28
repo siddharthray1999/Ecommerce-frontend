95942014266393f77861a4b69bd433c3e4bee60b
siddharth <siddharthray47@gmail.com>
2024-06-28 15:48:31 +0530

    git add

---
+++ e-commerce/src/App.js
@@ -0,0 +1,17 @@
+import Navigation from "./Customer/Components/Navigation/Navigation";
+
+
+function App() {
+  return (
+    <div className="">
+           <Navigation/>
+      <div>
+        home
+   
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
